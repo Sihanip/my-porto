995a12ce9b3c0add701c61f55ea3dc94ba5a8628
Sihanip <90297372+Sihanip@users.noreply.github.com>
2025-07-14 17:23:59 +0700

    FINAL FIX

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import commonjs from '@rollup/plugin-commonjs'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import commonjs from '@rollup/plugin-commonjs';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -17,10 +17,10 @@ export default defineConfig({
     outDir: 'dist'
   },
   optimizeDeps: {
-    include: ['cookie']
+    include: ['react', 'react-dom', 'lucide-react', 'cookie']
   },
   ssr: {
-    noExternal: ['react-router', 'cookie']
+    noExternal: ['react-router', 'cookie', 'lucide-react']
   },
   base: './'
-})
+});
